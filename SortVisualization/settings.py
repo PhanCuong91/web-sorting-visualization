fef6d6a07031ca4476bc0e7064838ad54f3ccfe2
phancuong91 <phancuong4320@gmail.com>
2020-11-07 21:49:31 +0800

    change project name according to folder name

--- SortVisualization/settings.py
+++ SortVisualization/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'SortVisulization.urls'
+ROOT_URLCONF = 'SortVisualization.urls'
 
 TEMPLATES = [
     {
